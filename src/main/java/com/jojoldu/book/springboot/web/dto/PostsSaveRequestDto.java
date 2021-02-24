5cc3d1cff58dbd6893497788f849350c8631ef41
seilylook@naver.com <seilylook@naver.com>
2021-02-24 15:19:49 +0900

    4 commit

--- src/main/java/com/jojoldu/book/springboot/web/dto/PostsSaveRequestDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -1,4 +1,30 @@
 package com.jojoldu.book.springboot.web.dto;
 
+import com.jojoldu.book.springboot.domain.posts.Posts;
+import com.jojoldu.book.springboot.service.posts.PostsService;
+import lombok.Builder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class PostsSaveRequestDto {
+
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
 }
