0fd2ead4c7b3d88d141bc4663e093f547bf0d85e
seilylook@naver.com <seilylook@naver.com>
2021-03-07 19:52:42 +0900

    11 commit

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -16,6 +16,12 @@ var main = {
 
     $.ajax({
         type: 'POST',
-
-    })
+        url: '/api/v1/posts'.
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(data)
+    }).done(function){
+        alert('글이 등록되었습니다.');
+        window.location.href='/';
+    }
 }
