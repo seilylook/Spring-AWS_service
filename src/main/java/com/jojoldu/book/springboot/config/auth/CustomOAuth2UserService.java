e6f6243bc3f1e1f80be531650cb00507fabe1b42
seilylook@naver.com <seilylook@naver.com>
2021-03-11 14:47:40 +0900

    google-login-service-2

--- src/main/java/com/jojoldu/book/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -2,8 +2,10 @@ package com.jojoldu.book.springboot.config.auth;
 
 import com.jojoldu.book.springboot.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,18 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
     private final UserRepository userRepository;
     private final HttpSession httpSession;
 
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest)throws OAuth2AuthenticationException {
 
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+
+    }
 
 }
